dcb9ccb052185183200a30c0c4112856164b2fd6
parksb <parkgds@gmail.com>
2026-02-24 21:54:29 +0900

    Load fullchain.pem

--- src/cert.rs
+++ src/cert.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
-use log::info;
+use log::{info, warn};
+use pingora::tls::ext;
 use pingora::tls::pkey::{PKey, Private};
 use pingora::tls::x509::X509;
 use pingora::{
@@ -12,44 +13,63 @@ use std::collections::HashMap;
 use crate::app::App;
 
 pub struct DynamicCert {
-    registry: HashMap<String, (X509, PKey<Private>)>,
+    registry: HashMap<String, (Vec<X509>, PKey<Private>)>,
 }
 
 impl DynamicCert {
     pub fn new(configs: Vec<App>) -> Self {
-        Self {
-            registry: configs
-                .into_iter()
-                .filter(|app| app.tls.is_some())
-                .map(|app| {
-                    let tls = app.tls.unwrap();
-                    info!(
-                        "Loading TLS key and cert from {}, {}",
-                        tls.cert_path, tls.key_path
-                    );
+        let mut registry = HashMap::new();
 
-                    let cert_bytes = std::fs::read(tls.cert_path).unwrap();
-                    let cert = X509::from_pem(&cert_bytes).unwrap();
+        for app in configs.into_iter().filter(|a| a.tls.is_some()) {
+            let tls = app.tls.unwrap();
 
-                    let key_bytes = std::fs::read(tls.key_path).unwrap();
-                    let key = PKey::private_key_from_pem(&key_bytes).unwrap();
+            info!(
+                "Loading TLS key and cert from {}, {}",
+                tls.cert_path, tls.key_path
+            );
 
-                    (app.hostname, (cert, key))
-                })
-                .collect::<HashMap<String, (X509, PKey<Private>)>>(),
+            let cert_bytes =
+                std::fs::read(&tls.cert_path).expect("Failed to read certificate file");
+
+            let cert_chain =
+                X509::stack_from_pem(&cert_bytes).expect("Failed to parse fullchain.pem");
+
+            if cert_chain.is_empty() {
+                panic!("Certificate chain is empty");
+            }
+
+            let key_bytes = std::fs::read(&tls.key_path).expect("Failed to read private key file");
+
+            let key = PKey::private_key_from_pem(&key_bytes).expect("Failed to parse private key");
+
+            registry.insert(app.hostname.to_ascii_lowercase(), (cert_chain, key));
         }
+
+        Self { registry }
     }
 }
 
 #[async_trait]
 impl TlsAccept for DynamicCert {
     async fn certificate_callback(&self, ssl: &mut SslRef) {
-        use pingora::tls::ext;
+        let sni = match ssl.servername(NameType::HOST_NAME) {
+            Some(name) => name.to_ascii_lowercase(),
+            None => {
+                warn!("TLS handshake without SNI");
+                return;
+            }
+        };
 
-        let sni = ssl.servername(NameType::HOST_NAME).unwrap();
-        if let Some((cert, key)) = self.registry.get(sni) {
-            ext::ssl_use_certificate(ssl, cert).unwrap();
-            ext::ssl_use_private_key(ssl, key).unwrap();
+        if let Some((cert_chain, key)) = self.registry.get(&sni) {
+            ext::ssl_use_certificate(ssl, &cert_chain[0]).expect("Failed to set leaf certificate");
+
+            ext::ssl_use_private_key(ssl, key).expect("Failed to set private key");
+
+            for cert in cert_chain.iter().skip(1) {
+                ext::ssl_add_chain_cert(ssl, cert).expect("Failed to add intermediate cert");
+            }
+        } else {
+            warn!("No certificate found for SNI: {}", sni);
         }
     }
 }
